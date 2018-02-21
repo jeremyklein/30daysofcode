902e857ce9526b1a709567316a2f66a0c380c959
jeremyklein <jeremy.joseph.klein@gmail.com>
2018-02-20 20:49:23 -0500

    added commit streak calculation

--- day1/commit_reminder.py
+++ day1/commit_reminder.py
@@ -9,8 +9,8 @@ with open("creds.json") as creds_json:
 
 def main():
 	username = creds["github_username"]
-	send_text(check_commits_today(username))
-
+	# send_text(check_commits_today(username))
+	check_commit_streak_today(username)
 
 def check_commits_today(username):
 	tz = timezone('EST')
@@ -21,6 +21,11 @@ def check_commits_today(username):
 	else:
 		return("Keep up the good work today mate!")
 
+def check_commit_streak_today(username):
+	tz = timezone('EST')
+	today = datetime.datetime.now(tz).strftime('%Y-%m-%d')	
+	commit_streak = commit_streak_for_date(username,today)
+	print(commit_streak) 
 
 def commit_count_for_date(username,search_date):
 	# scrape the github page, and check commit streak display
@@ -32,6 +37,25 @@ def commit_count_for_date(username,search_date):
 	for rectangle in rectangles:
 		if rectangle['data-date'] == search_date:
 			return int(rectangle['data-count'])
+
+
+def commit_streak_for_date(username,search_date):
+	# scrape the github page, and check commit streak display
+	url = "https://github.com/%s" % username
+	response = r.get(url)
+	html_doc = response.content
+	soup = BeautifulSoup(html_doc, 'html.parser')
+	rectangles = soup.find_all('rect')
+	streak = 0
+	for rectangle in rectangles:
+		if int(rectangle['data-count']) > 0:
+			streak += 1
+		else:
+			if rectangle['data-date'] == search_date:
+				return streak	
+			else:
+				streak = 0
+
 
 def send_text(message):
 	sid = creds["sid"]
